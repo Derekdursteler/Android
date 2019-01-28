e2a2bf0a2ebd8e5264fb6a44050577969950381d
derekdursteler <derekdursteler@gmail.com>
2019-01-28 14:52:16 -0700

    Captured input value

--- Project1/app/src/main/java/com/example/ddursteler1/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/ddursteler1/project1/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.ddursteler1.project1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mInputNumber;
     private Button mRoll;
     private Button mRollPicture;
-    private String mGuessNumber;
+    private int mGuessNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +26,18 @@ public class MainActivity extends AppCompatActivity {
         mRollPicture = findViewById(R.id.rollpicture);
 
         mInputNumber = findViewById(R.id.input_number);
-        mInputNumber.setOnClickListener(new View.OnClickListener() {
+        mInputNumber.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-                mGuessNumber = "0";
-                Log.d("InputNumber", mGuessNumber);
-
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    mGuessNumber = Integer.parseInt(mInputNumber.getText().toString());
+                    Log.d("EditText", mInputNumber.getText().toString());
+                    handled = true;
+                }
+                return handled;
             }
         });
+        Log.d("EditTextOutside", Integer.toString(mGuessNumber));
     }
 }
