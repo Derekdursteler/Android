ad4409f09362c25e697fab26e9dbbe995c9106a7
derekdursteler <derekdursteler@gmail.com>
2019-02-01 12:51:37 -0700

    Got results to show something

--- Project1/app/src/main/java/com/example/ddursteler1/project1/ScoreActivity.java
+++ Project1/app/src/main/java/com/example/ddursteler1/project1/ScoreActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
     private static final String GUESSED_NUMBER = "com.example.ddursteler1.project1.guessed_number";
 
     private int mGuessedNumber;
+    private TextView mGuesses;
+
 
     public static Intent newIntent(Context packageContext, int mGuessNumber) {
         Intent intent = new Intent(packageContext, ScoreActivity.class);
@@ -25,5 +28,9 @@ public class ScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score);
 
         mGuessedNumber = getIntent().getIntExtra(GUESSED_NUMBER, 0);
+        Log.d("Guessed number", Integer.toString(mGuessedNumber));
+
+        mGuesses = findViewById(R.id.guessed_numbers);
+        mGuesses.setText(Integer.toString(mGuessedNumber));
     }
 }
