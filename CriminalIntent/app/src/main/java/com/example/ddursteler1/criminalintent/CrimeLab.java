0f7426ec1b65e7b622c53a240685b50259f01628
derekdursteler <derekdursteler@gmail.com>
2019-02-11 14:54:14 -0700

    Starting recyclerview

--- CriminalIntent/app/src/main/java/com/example/ddursteler1/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/example/ddursteler1/criminalintent/CrimeLab.java
@@ -1,18 +1,24 @@
 package com.example.ddursteler1.criminalintent;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class CrimeLab {
-    private static final CrimeLab sCrimeLab = new CrimeLab();
+    private static CrimeLab sCrimeLab;
+
     private List<Crime> mCrimes;
 
-    public static CrimeLab get() {
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
         return sCrimeLab;
     }
 
-    private CrimeLab() {
+    private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
