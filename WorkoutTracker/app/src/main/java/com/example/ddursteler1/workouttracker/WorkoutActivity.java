1957cd488df9f8e781475661346d4593c353b0d3
derekdursteler <derekdursteler@gmail.com>
2019-02-27 12:50:44 -0700

    Ch8 next

--- WorkoutTracker/app/src/main/java/com/example/ddursteler1/workouttracker/WorkoutActivity.java
+++ WorkoutTracker/app/src/main/java/com/example/ddursteler1/workouttracker/WorkoutActivity.java
@@ -1,5 +1,7 @@
 package com.example.ddursteler1.workouttracker;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class WorkoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new WorkoutFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
---
+++ WorkoutTracker/app/src/main/java/com/example/ddursteler1/workouttracker/WorkoutFragment.java
@@ -0,0 +1,45 @@
+package com.example.ddursteler1.workouttracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class WorkoutFragment extends Fragment {
+    private WorkoutPlan mWorkoutPlan;
+    private EditText mDeadlift;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWorkoutPlan = new WorkoutPlan();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_workout, container, false);
+
+        mDeadlift = v.findViewById(R.id.deadlift);
+        mDeadlift.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mWorkoutPlan.setmDeadlift(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
+    }
+}
