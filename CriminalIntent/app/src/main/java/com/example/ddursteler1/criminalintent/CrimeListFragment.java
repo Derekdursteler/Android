eb503e0d09ed188d466a404d64230440fc78ba80
DerekDursteler <derekdursteler@gmail.com>
2019-03-21 09:33:01 -0600

    DB

--- CriminalIntent/app/src/main/java/com/example/ddursteler1/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/ddursteler1/criminalintent/CrimeListFragment.java
@@ -115,6 +115,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
         updateSubtitle();
@@ -176,5 +177,11 @@ public class CrimeListFragment extends Fragment {
         public int getItemCount() {
             return mCrimes.size();
         }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
     }
+
 }
